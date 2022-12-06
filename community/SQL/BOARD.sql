128b40cf4f16aec7c341f5e2181d3c2529891a48
지윤 <dlwldbs369@tukorea.ac.kr>
2022-12-06 16:03:19 +0900

    board sql update
    board sql update

--- community/SQL/BOARD.sql
+++ community/SQL/BOARD.sql
@@ -1,7 +1,13 @@
 create table board (
-   BOARD_ID int primary key auto_increment,
-   USER_EMAIL VARCHAR(20) NOT NULL,
-   TITLE VARCHAR(50) NOT NULL,
-   CONTENT_HTML longtext,
-   CONTENT_MARK longtext
+    BOARD_ID int primary key auto_increment,
+    USER_ID INT NOT NULL,
+    TITLE VARCHAR(50) NULL,
+    CONTENT_HTML longtext,
+    CONTENT_MARK longtext,
+    REPRE_IMAGE longtext,
+    TAG longtext,
+    STATE boolean,
+    RATING INT,
+    BOARD_DATE DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (`USER_ID`) REFERENCES `USER` (`USER_ID`) ON DELETE CASCADE
 );
